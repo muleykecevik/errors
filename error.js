49565a393cfed601a17ae15ae70b2acf8349abf2
muleykecevik <muleyke.cevik@gmail.com>
2024-03-07 13:06:39 +0300

    7/3/24

--- error.js
+++ error.js
@@ -67,6 +67,26 @@ app.get('/async', async (req, res, next) => {
 })
 
 /* ------------------------------------------------------- *
+
+app.get('/*', (req, res, next) => {
+
+    res.errorStatusCode = 404
+    throw new Error('There is an Error Message', { cause: 'No reason :)' })
+})
+
+/* ------------------------------------------------------- *
+
+const asyncFunction = async () => {
+    throw new Error('Error in async-function')
+}
+//? Control with catch(next)
+app.get('/async', async (req, res, next) => {
+    // await asyncFunction().then().catch((err) => { next(err) })) // Catch error by errorHandler.
+    await asyncFunction().then().catch(next) // Catch error by errorHandler.
+})
+
+
+/* ------------------------------------------------------- *
 //* ERROR HANDLER
 
 //? errorHandler is middleware and has must be four parameters. (error, request, response, next)
